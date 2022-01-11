31f07ef5d79bd12d0614d1e5260a63c231a30092
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-11 18:28:51 +0800

    role-permission seeder

--- src/entity/role.entity.ts
+++ src/entity/role.entity.ts
@@ -1,4 +1,5 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Permission} from "./permission.entity";
 
 @Entity()
 export class Role {
@@ -7,4 +8,12 @@ export class Role {
 
     @Column()
     name: string;
+
+    @ManyToMany(() => Permission)
+    @JoinTable({
+        name: 'role_permissions',
+        joinColumn: {name: 'role_id', referencedColumnName: 'id'},
+        inverseJoinColumn: {name: 'permission_id', referencedColumnName: 'id'}
+    })
+    permissions: Permission[];
 }
---
+++ src/seeder/role.seeder.ts
@@ -0,0 +1,51 @@
+import {createConnection, getManager} from "typeorm";
+import {Permission} from "../entity/permission.entity";
+import {Role} from "../entity/role.entity";
+
+
+createConnection().then(async () => {
+    const permissionRepository = getManager().getRepository(Permission);
+    const roleRepository = getManager().getRepository(Role)
+    const perms = [
+        'view_users',
+        'edit_users',
+        'view_roles',
+        'edit_roles',
+        'view_products',
+        'edit_products',
+        'view_orders',
+        'edit_orders'
+    ];
+    const permissions = [];
+
+    for(let i = 0;  i < perms.length; i++) {
+        permissions.push(
+            await permissionRepository.save({
+                name: perms[i]
+            })
+        );
+    }
+
+    await roleRepository.save({
+        name: 'Admin',
+        permissions
+    });
+
+    delete permissions[3];
+
+    await roleRepository.save({
+        name: 'Editor',
+        permissions
+    });
+
+    delete permissions[1];
+    delete permissions[5];
+    delete permissions[7];
+
+    await roleRepository.save({
+        name: 'Viewer',
+        permissions
+    })
+
+   process.exit(0);
+});
